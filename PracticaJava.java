4c9dddc8963c121cee6a98736fe3402ced72fb8e
geradevtab <everythingissofast@gmail.com>
2023-03-08 14:13:37 -0600

    inicio de metodos de los ejercicios

---
+++ PracticaJava.java
@@ -0,0 +1,16 @@
+//ejercicios de clase de java 2023 03 08
+
+package practicaJava1;
+
+public class Practica {
+
+	static void primerMetodo(int Numero12) {
+		System.out.println(Numero12);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		primerMetodo(11);
+	}
+
+}
